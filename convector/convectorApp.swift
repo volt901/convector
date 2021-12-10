98f4ddb3803c7edc85183d7d387f4fa2422f3e53
vladimir <volt901@icloud.com>
2021-12-10 13:47:08 +0500

    Initial Commit

---
+++ convector/convectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  convectorApp.swift
+//  convector
+//
+//  Created by Владимир Ширяев on 10.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct convectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
